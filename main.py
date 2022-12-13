2bfbf22d6d6fe56721102adfca65c894257115ae
RekhaJadhav26 <rekhanagawade26@gmail.com>
2022-12-13 10:59:00 +0530

    This is word file

---
+++ main.py
@@ -0,0 +1,5 @@
+Git is a distributed version control system: tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development. Its goals include speed, data integrity, and support for distributed, non-linear workflows.
+
+Create your first repository, then add and commit files At the command line, first verify that you have Git installed:
+On all operating systems:
+
